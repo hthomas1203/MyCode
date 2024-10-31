9611a1cc11db890a8e62b50e82815d3473f9ab12
Henry Thomas <101464914+hthomas1203@users.noreply.github.com>
2024-10-31 10:22:31 -0400

    Create firstbool.py

---
+++ firstbool.py
@@ -0,0 +1,8 @@
+"""
+This program should declare a boolean that describes whether or
+not you have a cat. Then you should print out an informative
+message to the user.
+
+"""
+cat = True
+print("You have a cat: " + str(cat))
