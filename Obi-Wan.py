44eaa5ef518f75d4759d8cc0ddc5fb7a91aa0975
Henry Thomas <101464914+hthomas1203@users.noreply.github.com>
2024-10-31 10:17:07 -0400

    Create Obi-Wan.py

---
+++ Obi-Wan.py
@@ -0,0 +1,4 @@
+name = input("What is your name?: ")
+droids = int(input("How many droids would you like to meet?: "))
+wookiees = int(input("How many Wookiees would you like to meet?: "))
+print(name + " wants to meet " + str(droids) + " droids " + "and " + str(wookiees) + " Wookiees." )
