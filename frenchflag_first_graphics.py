e7a7dcc781cc509b44dcd38fe1adf3b778136e22
Henry Thomas <101464914+hthomas1203@users.noreply.github.com>
2024-10-31 10:20:56 -0400

    Create frenchflag_first_graphics.py

---
+++ frenchflag_first_graphics.py
@@ -0,0 +1,10 @@
+blue_rect = Rectangle(get_width()/3, get_height())
+blue_rect.set_color(Color.blue)
+blue_rect.set_position(0, 0)
+add(blue_rect)
+
+
+red_rect = Rectangle(get_width()/3, get_height())
+red_rect.set_color(Color.red)
+red_rect.set_position(2 * get_width()/3, 0)
+add(red_rect)
